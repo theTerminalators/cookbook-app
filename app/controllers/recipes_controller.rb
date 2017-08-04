b67f9b75633d1bb0a050bf0564fb599d683452f1
Brian <riceb53@gmail.com>
2017-08-03 22:25:33 -0400

    Make form to create new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,16 @@ class RecipesController < ApplicationController
 
     render 'one_recipe_view.html.erb'
   end
+
+  def show
+    #  show the user the form
+    render 'show.html.erb'
+  end
+
+  def make
+    # where the form goes once it's submitted
+    recipe1 = Recipe.new(chef: params[:chef], ingredients: params[:ingredients], prep_time: params[:prep_time])
+    recipe1.save
+    render 'create.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   get '/one_recipe_url' => 'recipes#one_recipe_method'
+  get '/new_recipe' => 'recipes#show'
+  post '/make_the_recipe' => 'recipes#make'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# make a form
+# user fills our form with info to make new recipe
+# that info is sent through params to another action/url/whatever
+# in the different action, I will grab that user info from params and magically make a new recipe in the database
+
+
+
+
+# Begin the mini-capstone.

4f29d9e1450ed79eace8f184ffce976df27b8273
Brian <riceb53@gmail.com>
2017-08-06 10:32:08 -0400

    Add comments to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/one_recipe_url' => 'recipes#one_recipe_method'
+  # show the user a form
   get '/new_recipe' => 'recipes#show'
+  # actually make the new recipe, this is the route the form is pointed at
   post '/make_the_recipe' => 'recipes#make'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
